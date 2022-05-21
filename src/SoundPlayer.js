a29ea3de802004fe741bf213235377e6e91865f1
Bauss <jakob.burger@gmx.net>
2022-05-21 17:53:41 +0200

    You can now pause, continue and reset sounds.

--- src/SoundPlayer.js
+++ src/SoundPlayer.js
@@ -5,6 +5,7 @@ export default function SoundPlayer({ aSound }) {
   const [play, exposedData] = useSound(aSound.soundFile, {
     onend: () => {
       setSeconds(0);
+      setIsPlaying(false);
     },
   });
 
@@ -12,6 +13,7 @@ export default function SoundPlayer({ aSound }) {
 
   const [seconds, setSeconds] = useState(0);
   const [intervalId, setIntervalId] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   useEffect(() => {
     if (intervalId > 0 && seconds >= theLengthInSeconds) {
@@ -27,9 +29,29 @@ export default function SoundPlayer({ aSound }) {
     play();
   }
 
+  function handlePlayClicked() {
+    if (isPlaying) {
+      exposedData.pause();
+      clearInterval(intervalId);
+    } else {
+      playSound();
+    }
+    setIsPlaying(!isPlaying);
+  }
+
+  function handleStopClicked() {
+    exposedData.stop();
+    setIsPlaying(false);
+    setSeconds(0);
+    clearInterval(intervalId);
+  }
+
   return (
     <>
-      <button onClick={playSound}>Play</button>
+      <button onClick={handlePlayClicked}>
+        {isPlaying ? "Pause" : "Play"}
+      </button>
+      <button onClick={handleStopClicked}>Reset</button>
       <label>{aSound.name}</label>
       <br></br>
       <label>
